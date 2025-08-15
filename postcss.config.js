3299490affb7e1d828375cc339577019392ab517
Rich Leo <hello@richleo.me>
2025-08-15 13:46:33 -0500

    fix storybook

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,23 @@
+const fontPathTransform = () => {
+	return {
+		postcssPlugin: "font-path-transform",
+		once(root) {
+			root.walkAtRules("font-face", (rule) => {
+				rule.walkDecls("src", (decl) => {
+					if (decl.value.includes("./Geist-VariableFont.ttf")) {
+						decl.value = decl.value.replace(
+							"./Geist-VariableFont.ttf",
+							"./fonts/Geist-VariableFont.ttf",
+						);
+					}
+				});
+			});
+		},
+	};
+};
+
+fontPathTransform.postcss = true;
+
 module.exports = {
-	plugins: [require("postcss-import")],
+	plugins: [require("postcss-import"), fontPathTransform()],
 };
