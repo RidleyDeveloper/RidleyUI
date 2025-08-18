e63b7fea9f4818925562af053221ff053fd77446
Rich Leo <hello@richleo.me>
2025-08-18 14:27:18 -0500

    export additional components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from "./components/RuiText";
 export * from "./components/RuiButton";
+export * from "./components/RuiBadge";
+export * from "./components/RuiInput";
+export * from "./components/RuiCard";
 // export other wrapped components as you add them
